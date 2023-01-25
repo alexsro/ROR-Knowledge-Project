067b0c6b4a9686d98a151034a7f5b4e4a76993d4
alex <alex000sander@gmail.com>
2023-01-24 22:58:11 -0300

    início listagem de usuários

--- app/javascript/pages/users/index.tsx
+++ app/javascript/pages/users/index.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import IUserProps from './userProps';
 // import { Container } from './styles';
 
-const users: React.FC = () => {
-  return <div />;
-}
+const UsersIndex: React.FC<IUserProps[]> = ({...data}) => {
+  const [users, setUsers] = useState<IUserProps[]>([])
 
-export default users;
+  useEffect(() => {
+    const user_data = Object.values(data) as IUserProps[]
+    console.log(user_data)
+    setUsers(user_data)
+  }, [])
+
+  return (
+    <>
+      {users.map((v) => (
+        <div key={v.name}>
+          <p>name: {v.name}</p>
+          <p>email: {v.email}</p>
+          <p>----------------</p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default UsersIndex;
---
+++ app/javascript/pages/users/userProps.tsx
@@ -0,0 +1,9 @@
+interface IUserProps {
+  name: string,
+  email: string,
+  password: string,
+  created_at: string,
+  updated_at: string
+}
+
+export default IUserProps
